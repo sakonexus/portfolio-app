32546cb815a7f77ce43c9450691f1e3fa5bd5d70
Sako <Sakonexus@gmail.com>
2025-08-16 20:56:18 -0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    port: 3000,
-    proxy: {
-      '/api': 'http://localhost:5000',
-    },
-  },
   define: {
     'process.env': {},
   },
-  assetsInclude: ['**/*.html'],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
